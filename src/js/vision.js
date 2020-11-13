1074f6d00d536bd35bf23b2af613ad678bab25ea
lucaszw <lucas.zeer@gmail.com>
2020-11-13 14:26:14 +0100

    update text animations

--- src/js/vision.js
+++ src/js/vision.js
@@ -4,30 +4,52 @@ import "appear/dist/appear";
 import anime from "animejs";
 
 class Vision {
-    constructor() {
-        this.contianer = null;
-    }
+  constructor() {
+    this.contianer = null;
+  }
 
-    draw() {
-        this.container = el(".vision-container", {innerHTML: visionHTML});
-        mount(document.body, this.container);
+  draw() {
+    this.container = el(".vision-container", { innerHTML: visionHTML });
+    mount(document.body, this.container);
 
-        let h1 = this.container.querySelector("h1");
-        let h2 = this.container.querySelector("h2");
-        // h1.style.transform = "scale(0.9)";
-        // h2.style.transform = "scale(0.9)";
+    let h1 = this.container.querySelector("h1");
+    let h2 = this.container.querySelector("h2");
+    // h1.style.transform = "scale(0.9)";
+    // h2.style.transform = "scale(0.9)";
 
-        appear({elements: () => [h1], appear: () => {
-            anime.timeline({loop: false})
-            .add({
-              targets: [h1,h2],
-              scale: [0, 1],
-              duration: 2500,
-              elasticity: 1000,
-              delay: (el, i) => 15 * (i+1)
-            });
-        }})
-    }
+    // Wrap every letter in a span
+    var textWrapper = document.querySelector(".ml14 .letters");
+    textWrapper.innerHTML = textWrapper.textContent.replace(
+        /\S/g,
+        "<span class='letter'>$&</span>"
+    );
+    
+    appear({
+      elements: () => [h1],
+      appear: () => {
+        anime
+          .timeline({ loop: false })
+          .add({
+            targets: ".ml14 .line",
+            scaleX: [0, 1],
+            opacity: [0.5, 1],
+            easing: "easeInOutExpo",
+            duration: 900,
+          })
+          .add({
+            targets: ".ml14 .letter",
+            opacity: [0, 1],
+            translateX: [40, 0],
+            translateZ: 0,
+            scaleX: [0.3, 1],
+            easing: "easeOutExpo",
+            duration: 800,
+            offset: "-=600",
+            delay: (el, i) => 25 * i,
+          }, 100);
+      },
+    });
+  }
 }
 
 export default Vision;
