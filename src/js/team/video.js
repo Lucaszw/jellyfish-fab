9ccb4bbd04b9d3dc37d8f515f8bf3eb19c254570
lucaszw <lucas.zeer@gmail.com>
2020-11-18 18:35:27 +0100

    fixup team video dimensions

--- src/js/team/video.js
+++ src/js/team/video.js
@@ -4,17 +4,18 @@ import videoHtml from '../../html/team/video.html';
 import randInt from 'random-int';
 
 const focusPoints = [
-    {width: "200%", right: ""},
+    {width: "250%", right: ""},
     {width: "100%", right: ""},
-    {width: "200%", right: "100%"}
+    {width: "250%", right: "150%"}
 ];
 
-const minWidth = 781;
+const minWidth = 800;
 
 class Video {
     constructor(parent) {
         this.parent = parent;
         window.addEventListener("resize", _.debounce(this.changeFocus.bind(this),500,{leading: false, trailing: true}));
+        setInterval(this.changeFocus.bind(this), 3000);
     }
     draw () {
         let { height } = document.body.getBoundingClientRect();
@@ -29,10 +30,10 @@ class Video {
 
     changeFocus() {
         if (!this.container) return;
-        const {width} = document.body.getBoundingClientRect();
         const video = this.container.querySelector("video");
+
         let focus;
-        if (width <= minWidth) {
+        if (window.innerWidth < window.innerHeight) {
             focus = focusPoints[randInt(0,2)];
         } else {
             focus = focusPoints[1];
