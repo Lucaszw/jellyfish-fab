0741a75f9aa321cc20dea4c7d5ff8912e78d8476
lucaszw <lucas.zeer@gmail.com>
2020-12-03 01:40:15 +0100

    add proper material + 3 point lighting

--- src/js/services/jellyfish.js
+++ src/js/services/jellyfish.js
@@ -1,110 +1,118 @@
 import { el, mount, setStyle } from "redom";
 
+import * as THREE from "three";
 
-import * as THREE from 'three';
-
-import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader';
-import {MTLLoader} from 'three/examples/jsm/loaders/MTLLoader';
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
 
 let camera, scene, renderer;
-let geometry, material;
+let geometry, material, manager;
 let materials, mesh, object;
 
-let mtlLoader = new MTLLoader();
+manager = new THREE.LoadingManager();
+
+let mtlLoader = new MTLLoader(manager);
 let objLoader = new OBJLoader();
 
 window.THREE = THREE;
 
 class JellyFish {
-    constructor(parent) {
-        this.parent = parent;
-    }
+  constructor(parent) {
+    this.parent = parent;
+  }
 
-    loadMaterials() {
-        return new Promise((res, rej) => {
-            mtlLoader.load('/assets/Jellyfish.mtl', (m) => {
-                m.preload();
-                res(m);
-            });
+  loadMaterials() {
+    return new Promise((res, rej) => {
+      mtlLoader.load("/assets/Jellyfish.mtl", (m) => {
+        m.preload();
+        res(m);
+      });
+    });
+  }
+
+  loadObject(materials) {
+    return new Promise((res, rej) => {
+      objLoader
+        .setMaterials(materials)
+        .load("/assets/Jellyfish.obj", (object) => {
+        
+        console.log("BEFORE")
+        object.traverse(function (child) {
+          if (child instanceof THREE.Mesh) {
+            // child.material = new THREE.MeshPhongMaterial( { color: 0xdddddd, specular: 0x009900, shininess: 30, flatShading: true } )
+            child.geometry.center();
+          }
         });
-    }
+        console.log("AFTER")
+        res(object);
+      });
+    });
+  }
 
-    loadObject(materials) {
-        console.log({materials});
-        objLoader.setMaterials(materials);
-        return new Promise((res, rej) => {
-            objLoader.load('/assets/Jellyfish.obj', (object) => {
-                object.traverse(function(child) {
-                    console.log({child});
-                    if (child instanceof THREE.Mesh) {
-                        // child.material.color = 0xffb830;
-                        // child.material = new THREE.MeshNormalMaterial();
-                        child.geometry.center();
-                    }
-                });
+  addLights() {
+    let lights = this.lights = [null, null, null];
 
-                res(object);
-            });
-        });
-    }
+	lights[0] = new THREE.DirectionalLight( 0xffffff, 1 );
 
-    addLights() {
-        let lights = [null,null,null];
+    lights[0].position.x = 0.5
+    lights[0].position.y = -0.8;
+    lights[0].position.z = -0.23;
+    lights[0].position.normalize();
+    scene.add( lights[0] );
 
-        lights[0] = new THREE.AmbientLight(0x00ff00, 0.3);
-        lights[0].position.set( 50, 50, 50 );
-        scene.add( lights[0] );
-        camera.add( lights[0] );
+    lights[1] = new THREE.PointLight( 0xffffff, 1 );
+    lights[1].position.normalize();
+    scene.add(lights[1]);
+    camera.add(lights[1]);
 
-        lights[1] = new THREE.PointLight( 0xff0000, 1 );
-        lights[1].position.set( 100, 0, 150 );
-        scene.add( lights[1] );
-        camera.add( lights[1] );
+	lights[2] = new THREE.DirectionalLight( 0xffffff, 1 );
+    lights[2].position.x = -0.5
+    lights[2].position.y = 0.8;
+    lights[2].position.z = 0.23;
+    lights[2].position.normalize();
+    scene.add( lights[2] );
+  }
+  async draw() {
+    this.container = el(".jelly-3d-container");
+    mount(this.parent, this.container);
 
-        lights[2] = new THREE.PointLight( 0xff0000, 1, 100 );
-        lights[2].position.set( 100, 50, 450 );
-        scene.add( lights[2] );
+    camera = new THREE.PerspectiveCamera(
+      70,
+      window.innerWidth / window.innerHeight,
+      0.001,
+      10000
+    );
+    camera.position.z = 250;
+    
+    scene = new THREE.Scene();
+    scene.background = null;
+    geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    materials = await this.loadMaterials(manager);
+    object = await this.loadObject(materials);
+    mesh = object.children[0];
+    material = new THREE.MeshNormalMaterial();
 
-    }
-    async draw() {
-        this.container = el('.jelly-3d-container');
-        mount(this.parent, this.container);
+    this.addLights();
 
-        camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.001, 10000 );
-        camera.position.z = 250;
-     
-        scene = new THREE.Scene();
-        scene.background = null;
-        console.log({scene});
-        geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-        materials = await this.loadMaterials();
+    scene.add(object);
+    scene.add(camera);
 
-        object = await this.loadObject(materials);
-        mesh = object.children[0];
-        material = new THREE.MeshNormalMaterial();
-        mesh.material = material;
+    renderer = new THREE.WebGLRenderer({alpha: false, antialias: true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-        scene.add( mesh );
-        
-        this.addLights();
+    this.container.appendChild(renderer.domElement);
 
-        renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        renderer.toneMapping = THREE.ReinhardToneMapping;
-        
-        this.container.appendChild( renderer.domElement );
+    this.animate();
+  }
 
-        this.animate();
-    }
+  animate() {
+    requestAnimationFrame(this.animate.bind(this));
 
-    animate() {
-        requestAnimationFrame( this.animate.bind(this) );
-        
-        object.rotation.x += 0.01;
-        object.rotation.y += 0.02;
-     
-        renderer.render( scene, camera );
-    }
+    // object.rotation.x += 0.01;
+    // object.rotation.y += 0.02;
+
+    renderer.render(scene, camera);
+  }
 }
 
 export default JellyFish;
